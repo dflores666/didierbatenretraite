9099e5adb5de3f08f13fad573b91e16d080e84f7
David F. <david.flores.dlc@outlook.com>
2026-01-19 11:37:06 +0100

    game

---
+++ assets/photos.js
@@ -0,0 +1,11 @@
+// Liste des photos de Didier disponibles
+// Ajoute ici les noms des fichiers photos que tu mets dans le dossier assets/
+const DIDIER_PHOTOS = [
+    'didier1.jpg',
+    'didier2.jpg',
+    'didier3.jpg',
+    // Ajoute autant de photos que tu veux !
+    // 'didier4.jpg',
+    // 'didier_vacances.jpg',
+    // 'didier_bureau.jpg',
+];
